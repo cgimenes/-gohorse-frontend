054610138e7690af1ab0f30fb4c673821b4cebb8
jefersoncapobianco <jefersoncapobianco@gmail.com>
2018-03-14 19:35:26 -0300

    Corrigindo problemas com ESLint

---
+++ .env.example.js
@@ -0,0 +1,24 @@
+export default {
+  appTitle: 'Application Title',
+  theme: {
+    dark: false,
+    clippedLeft: false,
+    miniVariant: true,
+    showSidebar: true
+  },
+  colors: {
+    // sidebar: 'secondary',
+    footer: 'secondary',
+    appBar: 'primary',
+    loginPage: 'primary'
+  },
+  appBar: {
+    icons: {
+      dark: false,
+      clippedLeft: false,
+      miniVariant: false,
+      footerFixed: false,
+      showSidebar: false
+    }
+  }
+}
