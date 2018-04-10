14f8713d609347432c88bf4c66e614f5196dfa14
Jeferson Capobianco <jefersoncapobianco@gmail.com>
2018-04-09 22:35:14 -0300

    Implementado toast com as menagens de erro que retornar do backend

--- src/modules/Core/http.js
+++ src/modules/Core/http.js
@@ -3,4 +3,19 @@ import axios from 'axios'
 
 axios.defaults.baseURL = Vue.prototype.$env.api_url
 
+axios.interceptors.response.use(response => Promise.resolve(response), (error) => {
+  switch (error.response.status) {
+    case 404:
+      Vue.toasted.error(error.response.data.message, {
+        icon : 'warning'
+      })
+      break;
+    default:
+      Vue.toasted.error(error.response.data.message, {
+        icon : 'clear'
+      })
+  }
+  return Promise.reject(error)
+});
+
 export default axios
