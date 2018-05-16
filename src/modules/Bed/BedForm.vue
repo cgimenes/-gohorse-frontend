b2ee35377b886d006a2b218fd4b0c9f03e966c3d
Andre Martins <andre.felipe272@gmail.com>
2018-05-16 19:51:02 -0300

    alterado BedService

---
+++ src/modules/Bed/BedForm.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-container grid-list-lg="grid-list-lg" fluid="fluid">
+    <v-layout row="row" wrap="wrap">
+      <v-flex xs12>
+        <v-card>
+          <v-container grid-list-lg="grid-list-lg" fluid="fluid">
+            <v-layout row="row" wrap="wrap">
+              <v-flex col xs12>
+                <h4 class="grey--text">Dados do Leito</h4>
+              </v-flex>
+              <v-flex col xs12 >
+                <v-text-field name="name" label="Código" id="code" v-model="bed.code" key="code"></v-text-field>
+                <!--Mudar daqui para baixo de veterinary para bed-->
+              </v-flex>
+              <v-flex col xs12 sm6="sm6">
+                <v-text-field name="crmv" label="CRMV" id="crmv" v-model="veterinary.crmv" key="crmv"></v-text-field>
+              </v-flex>
+              <v-flex col xs12 sm6="sm6">
+                <v-text-field type="date" name="birthDate" label="Data de Nascimento" id="birthDate" v-model="veterinary.birthDate" key="email"></v-text-field>
+              </v-flex>
+              <v-flex col xs12>
+                <h4 class="grey--text">Dados de contato</h4>
+              </v-flex>
+              <v-flex col xs12 sm8>
+                <v-text-field name="email" label="E-mail" id="email" v-model="veterinary.email" key="email"></v-text-field>
+              </v-flex>
+              <v-flex col xs12 sm4>
+                <phone-input label="Telefone" :model.sync="veterinary.phone" :key="veterinary.id"></phone-input>
+              </v-flex>
+              <v-flex col xs12>
+                <h4 class="grey--text">Endereço</h4>
+              </v-flex>
+              <v-flex col xs12>
+                <address-component :address="veterinary.address"></address-component>
+              </v-flex>
+              <v-flex col xs12 sm6="sm6">
+              </v-flex>
+              <v-flex col xs12>
+                <v-btn color="primary" @click="saveVeterinary()">Salvar</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+import VeterinariesService from './VeterinariesService'
+import PhoneInput from '../Form/Field/PhoneInput'
+import AddressComponent from '../Form/Address/AddressComponent'
+
+export default {
+  components: {PhoneInput, AddressComponent},
+  data () {
+    return {
+      veterinary: {
+        name: '',
+        phone: '',
+        crmv: '',
+        email: '',
+        birthDate: '',
+        address: {
+          number: null,
+          complement: null,
+          postalCode: {
+            code: null,
+            streetType: 'Rua',
+            streetName: '',
+            neighbourhood: '',
+            city: '',
+            state: '',
+            country: 'Brasil'
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    saveVeterinary () {
+      VeterinariesService.saveVeterinary(this.veterinary, (res) => {
+        this
+          .$router
+          .push('/veterinaries/')
+      })
+    },
+    getDataForEdit () {
+      VeterinariesService.getVeterinaryDetails(this.$route.params.id, (veterinary) => {
+        this.veterinary = veterinary
+      })
+    }
+  },
+  created () {
+    if (this.$route.params.id) {
+      this.getDataForEdit()
+    }
+  }
+}
+</script>
---
+++ src/modules/Bed/VeterinaryRouter.js
@@ -0,0 +1,45 @@
+import Layout from '@core/components/Layout'
+import VeterinaryList from './VeterinaryList'
+import VeterinaryForm from './VeterinaryForm'
+import VeterinaryShow from './VeterinaryShow'
+
+export default [
+  {
+    path: '/veterinaries',
+    component: Layout,
+    children: [
+      {
+        path: '/veterinaries',
+        component: VeterinaryList,
+        meta: {
+          unrequiredAuth: true,
+          title: 'Veterinários'
+        }
+      },
+      {
+        path: '/veterinaries/create',
+        component: VeterinaryForm,
+        meta: {
+          unrequiredAuth: true,
+          title: 'Criar um novo Veterinário'
+        }
+      },
+      {
+        path: '/veterinaries/:id',
+        component: VeterinaryShow,
+        meta: {
+          unrequiredAuth: true,
+          title: 'Visualização de Veterinários'
+        }
+      },
+      {
+        path: '/veterinaries/:id/edit',
+        component: VeterinaryForm,
+        meta: {
+          unrequiredAuth: true,
+          title: 'Edição de Veterinários'
+        }
+      }
+    ]
+  }
+]
