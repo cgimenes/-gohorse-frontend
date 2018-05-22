218ab146ffb3a8b157bd0f7b7e90e419fa7974b4
alexandre.requena <alexandre.requena.95@gmail.com>
2018-05-22 19:14:03 -0300

    Criada a listagem de internamentos

---
+++ src/modules/Internments/InternmentsForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container grid-list-lg="grid-list-lg" fluid="fluid">
+    <v-layout row="row" wrap="wrap">
+      <v-flex xs12>
+        <v-card>
+          <v-container grid-list-lg="grid-list-lg" fluid="fluid">
+            <v-layout row="row" wrap="wrap">
+              <v-flex col xs12>
+                <h4 class="grey--text">Dados do Internamento</h4>
+              </v-flex>
+              <v-flex col xs12 sm6="sm6">
+                <v-text-field name="name" label="Nome do Laboratório" id="name" v-model="laboratory.companyName" key="name"></v-text-field>
+              </v-flex>
+              <v-flex col xs12 sm6="sm6">
+                <phone-input label="Telefone do Laboratório" :model.sync="laboratory.phone" :key="laboratory.id"></phone-input>
+              </v-flex>
+              <v-flex col xs12>
+                <h4 class="grey--text">Endereço</h4>
+              </v-flex>
+              <v-flex col xs12>
+                <address-component :address="laboratory.address"></address-component>
+              </v-flex>
+              <v-flex col xs12>
+                <v-btn color="primary" @click="saveInternment()">Salvar</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+
