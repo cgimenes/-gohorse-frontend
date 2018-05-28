d724f83283f881da1e711ee464be24c76e002032
Jeferson Capobianco <jefersoncapobianco@gmail.com>
2018-05-27 21:44:24 -0300

    Inserido exemplo de utilização de campo de data

--- src/modules/Internments/InternmentsForm.vue
+++ src/modules/Internments/InternmentsForm.vue
@@ -12,28 +12,28 @@
                 <v-text-field name="name" label="Nome do Animal" id="name" v-model="internment.animalId" key="name"></v-text-field>
               </v-flex>
               <v-flex col xs12 sm6="sm6">
-                <v-menu ref="menu" :close-on-content-click="false" v-model="menu" :nudge-right="40" :return-value.sync="date" lazy transition="scale-transition" offset-y full-width min-width="290px">
-                  <v-dialog ref="dialog" v-model="modal" :return-value.sync="date" persistent lazy full-width width="290px">
-                    <v-text-field slot="activator" v-model="date" label="Data de internamento" prepend-icon="event" readonly></v-text-field>
-                    <v-date-picker v-model="date" scrollable>
-                      <v-spacer></v-spacer>
-                      <v-btn flat color="primary" @click="modal = false">Cancelar</v-btn>
-                      <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
-                    </v-date-picker>
-                  </v-dialog>
-                </v-menu>
-              </v-flex>
-              <v-flex col xs12 sm6="sm6">
-                <v-menu ref="menu" :close-on-content-click="false" v-model="menu" :nudge-right="40" :return-value.sync="date" lazy transition="scale-transition" offset-y full-width min-width="290px">
-                  <v-dialog ref="dialog" v-model="modal" :return-value.sync="date" persistent lazy full-width width="290px">
-                    <v-text-field slot="activator" v-model="date" label="Data de liberação" prepend-icon="event" readonly></v-text-field>
-                    <v-date-picker v-model="date" scrollable>
-                      <v-spacer></v-spacer>
-                      <v-btn flat color="primary" @click="modal = false">Cancelar</v-btn>
-                      <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
-                    </v-date-picker>
-                  </v-dialog>
-                </v-menu>
+                <v-menu
+                 ref="menu"
+                 :close-on-content-click="false"
+                 v-model="menu"
+                 nudge-right="40"
+                 lazy
+                 transition="scale-transition"
+                 offset-y
+                 full-width
+                 max-width="290px"
+                 min-width="290px"
+               >
+                 <v-text-field
+                   slot="activator"
+                   v-model="internment.date"
+                   label="Date"
+                   hint="MM/DD/YYYY format"
+                   persistent-hint
+                   prepend-icon="event"
+                 ></v-text-field>
+                 <v-date-picker v-model="internment.date" no-title></v-date-picker>
+               </v-menu>
               </v-flex>
               <v-flex col xs12>
                 <v-btn color="primary" @click="saveInternment()">Salvar</v-btn>
@@ -42,6 +42,7 @@
           </v-container>
         </v-card>
       </v-flex>
+      {{ internment }}
     </v-layout>
   </v-container>
 </template>
@@ -58,8 +59,10 @@ export default {
           animalId: '',
           bedId: '',
           busyAt: '',//moment(Date.now()).format('DD/MM/YY HH:MM'),
-          busyUntil: ''
-      }
+          busyUntil: '',
+          date: null
+      },
+      menu: false
     }
   },
   methods: {
@@ -75,7 +78,7 @@ export default {
       InternmentsService.getInternmentDetails(this.$route.params.id, (internment) => {
         this.internment = internment
       })
-    }    
+    }
   },
   created() {
     if(this.$route.params.id){
@@ -84,4 +87,3 @@ export default {
   }
 }
 </script>
-
