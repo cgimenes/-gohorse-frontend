f8a0a53b766eddc8bb8d061946e92054b24f7b6e
André Fernando da Conceição <andre.fer.conceicao@gmail.com>
2018-10-02 19:38:19 -0300

    Alterado cores do sistema, convertido linguagem PUG utilizada antigamente para HTMl

--- src/modules/Dashboard/Dashboard.vue
+++ src/modules/Dashboard/Dashboard.vue
@@ -1,34 +1,50 @@
-<template lang="pug">
-  v-container(grid-list-lg fluid)
-    v-layout(row wrap)
-      v-flex(sm12 md6 lg3)
-        indicador(title="Consultas Agendadas" value="1000" color="teal" icon="assignment")
-      v-flex(sm12 md6 lg3)
-        indicador(title="Consultas Agendadas" value="1000" color="orange")
-      v-flex(sm12 md6 lg3)
-        indicador(title="Consultas Agendadas" value="1000" color="purple" icon="assignment")
-      v-flex(sm12 md6 lg3)
-        indicador(title="Consultas Agendadas" value="1000" color="blue")
-      v-flex(sm6)
-        v-card
-          v-card-title Olá
-          v-card-text
-            grafico-barras(height="300")
-      v-flex(sm6)
-        v-card
-          v-card-title Olá
-          v-card-text
-            grafico-pizza(height="300")
-      v-flex(sm12)
-        grafico-linhas(max-height="200px")
-      v-flex(sm4)
-        v-card
-          v-card-title Olá
-          v-card-text Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
-      v-flex(sm8)
-        v-card
-          v-card-title Olá
-          v-card-text Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<template>
+  <v-container grid-list-lg="grid-list-lg" fluid="fluid">
+    <v-layout row="row" wrap="wrap">
+      <v-flex sm12="sm12" md6="md6" lg4="lg4">
+        <indicador
+          title="Consultas Agendadas"
+          value="1000"
+          color="deep-purple darken-1"
+          icon="assignment">
+        </indicador>
+      </v-flex>
+      <v-flex sm12="sm12" md6="md6" lg4="lg4">
+        <indicador
+          title="Consultas Hoje"
+          value="1000"
+          color="pink darken-1"
+          icon="today">
+        </indicador>
+      </v-flex>
+      <v-flex sm12="sm12" md6="md6" lg4="lg4">
+        <indicador
+          title="Internamentos"
+          value="1000"
+          color="deep-orange darken-2"
+          icon="list">
+        </indicador>
+      </v-flex>
+      <v-flex sm6="sm6">
+        <v-card>
+          <v-card-title>
+          </v-card-title>
+          <v-card-text>
+            <grafico-barras></grafico-barras>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex sm6="sm6">
+        <v-card>
+          <v-card-title>
+          </v-card-title>
+          <v-card-text>
+            <grafico-pizza></grafico-pizza>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -41,7 +57,7 @@ export default {
   components: {Indicador, GraficoLinhas, GraficoPizza, GraficoBarras},
   data () {
     return {
-      msg: 'Welcome to Your Modular Vue.js App'
+      msg: 'Bem vindo ao GoHorse'
     }
   }
 }
