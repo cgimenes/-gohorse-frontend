ec0b8dfd662f925f016e7f328a53a8da0c5d62e7
Unknown <andre.felipe272@gmail.com>
2018-10-13 01:23:27 -0300

    #13067 - Criado componentes do gerenciamento de produtos

---
+++ src/modules/Products/ProductsForm.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-container grid-list-lg="grid-list-lg" fluid="fluid">
+        <v-layout row="row" wrap="wrap">
+            <v-flex xs12>
+                <v-card>
+                    <v-container grid-list-lg="grid-list-lg" fluid="fluid">
+                        <v-layout row="row" wrap="wrap">
+                            <v-flex col xs12>
+                                <h4 class="grey--text">Dados do Animal</h4>
+                            </v-flex>
+                            <v-flex col xs12>
+                                <v-text-field name="name" label="Nome do Animal" id="name" v-model="animal.name" key="name"></v-text-field>
+                            </v-flex>
+                            <v-flex col xs12 sm6="sm6">
+                              <v-text-field type="date" name="birthDate" label="Data de Nascimento" id="birthDate" v-model="animal.birthDate" key="birthDate"></v-text-field>
+                            </v-flex>
+                            <v-flex col xs12 sm6="sm6">
+                              <autocomplete label="Proprietário do Animal" :model.sync="animal.owner" :key="animal.id"></autocomplete>
+                            </v-flex>
+                            <v-flex col xs12 sm4="sm4">
+                                <v-text-field name="specie" label="Espécie do Animal" id="specie" v-model="animal.specie" key="specie"></v-text-field>
+                            </v-flex>
+                            <v-flex col xs12 sm4="sm4">
+                                <v-text-field name="breed" label="Raça do Animal" id="breed" v-model="animal.breed" key="breed"></v-text-field>
+                            </v-flex>
+                            <v-flex col xs12 sm4="sm4">
+                                <v-text-field name="sex" label="Sexo do Animal" id="sex" v-model="animal.sex" key="sex"></v-text-field>
+                            </v-flex>
+                            <v-flex col xs12>
+                                <v-btn color="primary" @click="saveAnimal()">Salvar</v-btn>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-card>
+            </v-flex>
+        </v-layout>
+</v-container>
+</template>
+
+<script>
+import ProductsService from './ProductsService'
+import Autocomplete from './Autocomplete'
+
+export default {
+  components: { Autocomplete },
+  data () {
+    return {
+      product: {
+        supplier: { //fornecedor esta dentro de produtos
+        }
+      }
+    }
+  },
+    methods: {
+        saveProduct () {
+                ProductsService.saveProduct(this.product, (res) => {
+                    this
+                        .$router
+                        .push('/products/')
+                })
+            },
+            getDataForEdit () {
+                ProductsService.getProductDetails(this.$route.params.id, (product) => {
+                    this.product = product
+                })
+            }
+    },
+    created () {
+        if (this.$route.params.id) {
+            this.getDataForEdit()
+        }
+    }
+}
+</script>
