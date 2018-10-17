3a4ac3b179a14b777e38b5dcc357b395502c50dd
Unknown <andre.felipe272@gmail.com>
2018-10-17 19:41:19 -0300

    #13067 - Adicionado Puzzle Pattern e novas propriedades para o text-field

--- src/modules/Products/ProductsForm.vue
+++ src/modules/Products/ProductsForm.vue
@@ -2,32 +2,77 @@
 <v-container grid-list-lg="grid-list-lg" fluid="fluid">
   <v-layout row="row" wrap="wrap">
     <v-flex xs12>
-      <v-card>
+      <v-card >
         <v-container grid-list-lg="grid-list-lg" fluid="fluid">
           <v-layout row="row" wrap="wrap">
             <v-flex col xs12>
-              <h4 class="grey--text">Dados do Produto</h4>
+              <h4 class="grey--text">Dados do produto</h4>
             </v-flex>
             <v-flex col xs12>
-              <v-text-field name="name" label="Nome do Produto" id="name" v-model="product.name" key="name"></v-text-field>
+              <v-text-field
+                id="name"
+                key="name"
+                name="name"
+                :rules="[rules.empty]"
+                v-model="product.name"
+                label="Nome do Produto"
+                placeholder="ex: Ração"
+                >
+              </v-text-field>
             </v-flex>
             <v-flex col xs12 sm6="sm6">
-              <v-text-field type="date" name="birthDate" label="Data de Nascimento" id="birthDate" v-model="product.birthDate" key="birthDate"></v-text-field>
+              <v-text-field
+                id="brand"
+                key="brand"
+                name="brand"
+                :rules="[rules.empty]"
+                v-model="product.brand"
+                label="Marca do produto"
+                placeholder="ex: Special Dog"
+                >
+              </v-text-field>
             </v-flex>
             <v-flex col xs12 sm6="sm6">
-              <autocomplete label="Proprietário do Produto" :model.sync="product.supplier" :key="product.id"></autocomplete>
+              <autocomplete
+              :key="product.id"
+                :rules="[rules.empty]"
+                label="Fornecedor do produto"
+                :model.sync="product.supplier"
+                >
+              </autocomplete>
             </v-flex>
-            <v-flex col xs12 sm4="sm4">
-              <v-text-field name="specie" label="Espécie do Produto" id="specie" v-model="product.specie" key="specie"></v-text-field>
+            <v-flex col xs12 sm6="sm6">
+              <v-text-field name="price"
+                id="price"
+                prefix="R$"
+                key="price"
+                placeholder="500,00"
+                :rules="[rules.empty]"
+                v-model="product.price"
+                label="Preço do produto"
+                >
+              </v-text-field>
             </v-flex>
-            <v-flex col xs12 sm4="sm4">
-              <v-text-field name="breed" label="Raça do Produto" id="breed" v-model="product.breed" key="breed"></v-text-field>
-            </v-flex>
-            <v-flex col xs12 sm4="sm4">
-              <v-text-field name="sex" label="Sexo do Produto" id="sex" v-model="product.sex" key="sex"></v-text-field>
+            <v-flex col xs12 sm6="sm6">
+              <v-text-field
+                min="0"
+                id="amount"
+                key="amount"
+                type="number"
+                mask="######"
+                name="amount"
+                label="Quantidade"
+                :rules="[rules.empty]"
+                v-model="product.amount"
+                >
+              </v-text-field>
             </v-flex>
             <v-flex col xs12>
-              <v-btn color="primary" @click="saveProduct()">Salvar</v-btn>
+              <v-btn
+                color="primary"
+                @click="saveProduct()"
+                >Salvar
+              </v-btn>
             </v-flex>
           </v-layout>
         </v-container>
@@ -50,6 +95,9 @@ export default {
       product: {
         supplier: { //fornecedor esta dentro de produtos
         }
+      },
+      rules: {
+        empty: value => (value || '').length > 0 || 'Preenchimento obrigatório!'
       }
     }
   },
