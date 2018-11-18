759674acb3b5cf4920a355a7c8495167763dea5b
João Paulo Trentini da Silva <joao.trentinii@gmail.com>
2018-11-18 20:05:27 -0200

    Revert "Revert "Correção na inserção e na edição do Animal e do Supplier""
    This reverts commit 60e27fffd1bbfb97053ceeb3a1efcfc7e8ff15d0.

--- src/modules/Suppliers/SuppliersForm.vue
+++ src/modules/Suppliers/SuppliersForm.vue
@@ -1,117 +1,99 @@
 <template>
-  <v-container grid-list-lg="grid-list-lg" fluid="fluid">
+<v-container grid-list-lg="grid-list-lg" fluid="fluid">
     <v-layout row="row" wrap="wrap">
-      <v-flex xs12>
-        <v-card>
-          <v-container grid-list-lg="grid-list-lg" fluid="fluid">
-            <v-layout row="row" wrap="wrap">
-              <v-flex col xs12>
-                <h4 class="grey--text">Dados do Fornecedor</h4>
-              </v-flex>
-              <v-flex col xs12 sm3="sm3">
-                <v-text-field name="name" label="Nome" id="name" v-model="supplier.name" key="name"></v-text-field>
-              </v-flex>
-              <v-flex col xs12 sm3="sm3">
-                <document-input label="CPF/CNPJ" :model.sync="supplier.document" :key="supplier.id"></document-input>
-              </v-flex>
-              <v-flex col xs12 sm3="sm3">
-                <phone-input label="Telefone" :model.sync="supplier.phone" :key="supplier.id"></phone-input>
-              </v-flex>
-              <v-flex col xs12 sm3="sm2">
-                <template>
-                  <v-layout row>
-                    <v-flex>
-                      <v-select
-                        :items="distributionTypes"
-                        label="Tipo de distribuição"
-                        item-value="text"
-                      ></v-select>
-                    </v-flex>
-                  </v-layout>
-                </template>
-
-              </v-flex>
-              <v-flex col xs12>
-                <h4 class="grey--text">Endereço</h4>
-              </v-flex>
-              <v-flex col xs12>
-                <address-component
-                  :address="supplier.address"
-                  :model.sync="supplier.address"
-                  :key="supplier.id"
-                  >
-                </address-component>
-              </v-flex>
-              <v-flex col xs12>
-              </v-flex>
-              <v-flex col xs12>
-                <v-btn color="primary" @click="saveSupplier()">Salvar</v-btn>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
-      </v-flex>
+        <v-flex xs12>
+            <v-card>
+                <v-container grid-list-lg="grid-list-lg" fluid="fluid">
+                    <v-layout row="row" wrap="wrap">
+                        <v-flex col xs12>
+                            <h4 class="grey--text">Dados do Fornecedor</h4>
+                        </v-flex>
+                        <v-flex col xs12 sm5="sm5">
+                            <v-text-field name="name" label="Nome" id="name" v-model="supplier.name" key="name"></v-text-field>
+                        </v-flex>
+                        <v-flex col xs12 sm2="sm2">
+                            <document-input label="CPF/CNPJ" :model.sync="supplier.document" :key="supplier.id"></document-input>
+                        </v-flex>
+                        <v-flex col xs12 sm2="sm2">
+                            <phone-input label="Telefone" :model.sync="supplier.phone" :key="supplier.id"></phone-input>
+                        </v-flex>
+                        <v-flex col xs12 sm3="sm3">
+                            <dist-complete label="Tipo de Distribuição" :distributionType="supplier.distributionType.id" :model.sync="supplier.distributionType" :key="supplier.id"></dist-complete>
+                        </v-flex>
+                        <v-flex col xs12>
+                            <h4 class="grey--text">Endereço</h4>
+                        </v-flex>
+                        <v-flex col xs12>
+                            <address-component :address="supplier.address" :model.sync="supplier.address" :key="supplier.id">
+                            </address-component>
+                        </v-flex>
+                        <v-flex col xs12>
+                        </v-flex>
+                        <v-flex col xs12>
+                            <v-btn color="primary" @click="saveSupplier()">Salvar</v-btn>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-card>
+        </v-flex>
     </v-layout>
-  </v-container>
+</v-container>
 </template>
 
 <script>
-  import SuppliersService from './SuppliersService'
-  import AddressComponent from '../Form/Address/AddressComponent'
-  import DocumentInput from '../Form/Field/DocumentInput'
-  import PhoneInput from '../Form/Field/PhoneInput'
+import SuppliersService from './SuppliersService'
+import AddressComponent from '../Form/Address/AddressComponent'
+import DocumentInput from '../Form/Field/DocumentInput'
+import PhoneInput from '../Form/Field/PhoneInput'
+import DistComplete from '../Enumerators/AutocompleteDistType'
 
-  export default {
+export default {
     components: {
-      AddressComponent,
-      PhoneInput,
-      DocumentInput
+        AddressComponent,
+        PhoneInput,
+        DocumentInput,
+        DistComplete
     },
-    data () {
-      return {
-        distributionTypes: [],
-        supplier: {
-          address: {
-            number: null,
-            complement: null,
-            postalCode: {
-              code: null,
-              streetType: 'Rua',
-              streetName: '',
-              neighbourhood: '',
-              city: '',
-              state: '',
-              country: 'Brasil'
+    data() {
+        return {
+            supplier: {
+                address: {
+                  number: null,
+                  complement: null,
+                  postalCode: {
+                    code: null,
+                    streetType: 'Rua',
+                    streetName: '',
+                    neighbourhood: '',
+                    city: '',
+                    state: '',
+                    country: 'Brasil'
+                  }
+                },
+                distributionType: {}
             }
-          },
-          document: '',
-          distributionType: {
-            value: 'A'
-          }
         }
-      }
     },
     methods: {
-      saveSupplier () {
-        SuppliersService.saveSupplier(this.supplier, (res) => {
-          this
-            .$router
-            .push('/suppliers/')
-        })
-      },
-      getDataForEdit () {
-        SuppliersService.getSupplierDetails(this.$route.params.id, (supplier) => {
-          this.supplier = supplier
-          supplier.distributionType = {
-            value: 'A'
-          }
-        })
-      }
+        saveSupplier() {
+                SuppliersService.saveSupplier(this.supplier, (res) => {
+                    this.$toasted.success('Fornecedor salvo com sucesso!', {icon: 'check'})
+                    this
+                        .$router
+                        .push('/suppliers/')
+                })
+            },
+            getDataForEdit() {
+                SuppliersService.getSupplierDetails(this.$route.params.id, (supplier) => {
+                    this.supplier = supplier
+                    this.supplier.distributionType = this.supplier.distributionType.id
+                })
+            }
     },
-    created () {
-      if (this.$route.params.id) {
-        this.getDataForEdit()
-      }
+    created() {
+        if (this.$route.params.id) {
+            this.getDataForEdit()
+        }
     }
-  }
+}
 </script>
